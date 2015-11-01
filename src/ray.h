5921e6d307d74a48c4b6b655887eacc10aca432b
Elijah Insua <tmpvar@gmail.com>
2015-11-01 16:49:24 -0500

    split off ray_classify

--- src/ray.h
+++ src/ray.h
@@ -37,34 +37,7 @@ typedef struct ray_t
   float c_xy, c_xz, c_yx, c_yz, c_zx, c_zy;
 } ray3;
 
-void ray_update(ray3 *r, vec3 ro, vec3 rd)
-{
-  //common variables
-  r->x = ro[0];
-  r->y = rd[1];
-  r->z = ro[2];
-  r->i = rd[0];
-  r->j = rd[1];
-  r->k = rd[2];
-
-  r->ii = 1.0f/r->i;
-  r->ij = 1.0f/r->j;
-  r->ik = 1.0f/r->k;
-
-  //ray slope
-  r->ibyj = r->i * r->ij;
-  r->jbyi = r->j * r->ii;
-  r->jbyk = r->j * r->ik;
-  r->kbyj = r->k * r->ij;
-  r->ibyk = r->i * r->ik;
-  r->kbyi = r->k * r->ii;
-  r->c_xy = r->y - r->jbyi * r->x;
-  r->c_xz = r->z - r->kbyi * r->x;
-  r->c_yx = r->x - r->ibyj * r->y;
-  r->c_yz = r->z - r->kbyj * r->y;
-  r->c_zx = r->x - r->ibyk * r->z;
-  r->c_zy = r->y - r->jbyk * r->z;
-
+void ray_classify(ray3 *r) {
   //ray slope classification
   if(r->i < 0)
   {
@@ -163,6 +136,37 @@ void ray_update(ray3 *r, vec3 ro, vec3 rd)
       }
     }
   }
+}
+
+void ray_update(ray3 *r, vec3 ro, vec3 rd)
+{
+  //common variables
+  r->x = ro[0];
+  r->y = rd[1];
+  r->z = ro[2];
+  r->i = rd[0];
+  r->j = rd[1];
+  r->k = rd[2];
+
+  r->ii = 1.0f/r->i;
+  r->ij = 1.0f/r->j;
+  r->ik = 1.0f/r->k;
+
+  //ray slope
+  r->ibyj = r->i * r->ij;
+  r->jbyi = r->j * r->ii;
+  r->jbyk = r->j * r->ik;
+  r->kbyj = r->k * r->ij;
+  r->ibyk = r->i * r->ik;
+  r->kbyi = r->k * r->ii;
+  r->c_xy = r->y - r->jbyi * r->x;
+  r->c_xz = r->z - r->kbyi * r->x;
+  r->c_yx = r->x - r->ibyj * r->y;
+  r->c_yz = r->z - r->kbyj * r->y;
+  r->c_zx = r->x - r->ibyk * r->z;
+  r->c_zy = r->y - r->jbyk * r->z;
+
+  ray_classify(r);
 }
 
 #endif
