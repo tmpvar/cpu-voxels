183369fa2e62e3a467b8c28655aa4bead3623cbf
Elijah Insua <tmpvar@gmail.com>
2015-11-18 23:56:06 -0500

    pack an aabb_packet whenever the brick moves

--- src/voxel.h
+++ src/voxel.h
@@ -17,6 +17,7 @@
     float voxels[BRICK_RADIUS][BRICK_RADIUS][BRICK_RADIUS];
     vec3 center;
     aabb bounds;
+    aabb_packet bounds_packet;
   } voxel_brick;
 
   void voxel_brick_set(voxel_brick brick, const unsigned int x, const unsigned int y, const unsigned int z, float v) {
@@ -42,10 +43,22 @@
   }
 
   static void voxel_brick_position(voxel_brick brick, const vec3 center) {
+    // position the brick in space
     brick.center = center;
+
+    // create a normal aabb
     vec3 corner = vec3f(VOXEL_SIZE * BRICK_HALF_RADIUS);
     brick.bounds[0] = _mm_sub_ps(center, corner);
     brick.bounds[1] = _mm_add_ps(center, corner);
+
+    // create an aabb packet
+    brick.bounds_packet[0] = _mm_set1_ps(brick.bounds[0][0]);
+    brick.bounds_packet[1] = _mm_set1_ps(brick.bounds[0][1]);
+    brick.bounds_packet[2] = _mm_set1_ps(brick.bounds[0][2]);
+    brick.bounds_packet[3] = _mm_set1_ps(brick.bounds[1][0]);
+    brick.bounds_packet[4] = _mm_set1_ps(brick.bounds[1][1]);
+    brick.bounds_packet[5] = _mm_set1_ps(brick.bounds[1][2]);
   }
+
 
 #endif
