2bc8e24f985bbc7e3070f4c0d729879aef6a3e06
Elijah Insua <tmpvar@gmail.com>
2015-11-30 21:37:22 -0700

    add ray_isect vs ray_isect_simd single ray intersection benchmarks

--- test/bench.cc
+++ test/bench.cc
@@ -1,19 +1,75 @@
+#include "ray.h"
+#include "aabb.h"
+#include "ray-aabb.h"
+#include "voxel.h"
+#include "voxel-scene.h"
+#include "vec.h"
+
 #include <string>
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-  while (state.KeepRunning())
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+static void bench_voxel_scene_traverse_miss(benchmark::State& state) {
+  int out, accumulator[3];
+  ray3 r;
+  vec3 center = vec3f(VOXEL_BRICK_HALF_SIZE);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  while (state.KeepRunning())
-    std::string copy(x);
+  r.origin = vec3_create(VOXEL_BRICK_HALF_SIZE, VOXEL_BRICK_HALF_SIZE, 1.0f);
+  r.dir = vec3_norm(center - r.origin);
+  r.invdir = vec3_reciprocal(r.dir);
+
+  voxel_brick brick = voxel_brick_create();
+
+  voxel_brick_position(brick, center);
+  voxel_brick_fill_constant(brick, 0.0f);
+
+  voxel_scene scene = voxel_scene_create();
+  voxel_scene_add_brick(scene, brick);
+
+  while (state.KeepRunning()) {
+    voxel_scene_ray(scene, &r, accumulator);
+  }
 }
-BENCHMARK(BM_StringCopy);
+
+BENCHMARK(bench_voxel_scene_traverse_miss);
+
+static void bench_ray_aabb(benchmark::State& state) {
+
+  ray3 r;
+  r.origin = vec3_create(0.0f, 0.0f, 1.0f);
+  r.dir = vec3_norm(-r.origin);
+  r.invdir = vec3_reciprocal(r.dir);
+
+  aabb b;
+  b[0] = vec3f(-1.0f);
+  b[1] = vec3f( 1.0f);
+
+  vec3 out;
+  while (state.KeepRunning()) {
+    ray_isect(&r, b, &out);
+  }
+}
+
+BENCHMARK(bench_ray_aabb);
+
+
+static void bench_ray_aabb_simd(benchmark::State& state) {
+
+  ray3 r;
+  r.origin = vec3_create(0.0f, 0.0f, 1.0f);
+  r.dir = vec3_norm(-r.origin);
+  r.invdir = vec3_reciprocal(r.dir);
+
+  aabb b;
+  b[0] = vec3f(-1.0f);
+  b[1] = vec3f( 1.0f);
+
+  vec3 out;
+  while (state.KeepRunning()) {
+    ray_isect_simd(&r, b, &out);
+    (void)out;
+  }
+}
+
+BENCHMARK(bench_ray_aabb_simd);
 
 BENCHMARK_MAIN();
